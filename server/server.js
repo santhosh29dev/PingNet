b58129e4c0734a31d66c89b3dc5ec5645d8d1116
AtlasBlackwood <santhosh29.dev@gmail.com>
2026-05-06 11:39:35 +0530

    build : server js with socket.io connection

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const http=require('http');
+const { Server, Socket }=require('socket.io');
+const mongoose=require('mongoose');
+const cors=require('cors')
+require('dotenv').config()
+
+const app=express();
+const server=http.createServer(app);
+const io=new Server(server,{
+    cors:{origin : "http://localhost:5173"}
+});
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("mongoDB connected");
+    
+}).catch((err)=>{
+    console.error(err);
+})
+
+io.on('connection' ,(socket)=>{
+    console.log('User connected',socket.id);
+
+    socket.on('join_room',(data)=>{
+        socket.join(data);
+        console.log(`User joined room: ${data}`);
+        
+    });
+
+    socket.on('send_message',async (data)=>{
+        const newMessage = new Message(data);
+        await newMessage.save();
+        socket.to(data.room).emit('recieve_message',data);
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('User disconnected');
+        
+    });
+    
+});
+
+
+const PORT=process.env.PORT || 5000;
+server.listen(PORT,()=>{{
+    console.log(`Server running on port ${PORT}`);
+}});
